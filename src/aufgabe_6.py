da8598e6337dcf7ee2cdd633bf46b6afe03bb5a8
StefanDataCraft <stefan.ronacher@data-craft-students.de>
2023-04-26 10:32:44 +0200

    First approach, class Dozent now working

--- src/aufgabe_6.py
+++ src/aufgabe_6.py
@@ -5,7 +5,25 @@ class Person:
     def introduce(self):
         print(f"Hallo, mein Name ist {self.name}")
 
-class Dozent extends
+
+class Dozent(Person):
+    skill_list = []
+
+    def __init__(self, name, biographie):
+        super().__init__(name)
+        self.biographie = biographie
+
+    def add_skill(self, skill_name):
+        self.skill_list.append(skill_name)
+
+    def get_skills(self):
+        return self.skill_list
+
+
 person1 = Person("Stepsi")
 person1.introduce()
 
+dozent1 = Dozent("Karli", "Ich bin ich, und du bist mein Sofa")
+dozent1.introduce()
+dozent1.add_skill("C++")
+print(dozent1.get_skills())
