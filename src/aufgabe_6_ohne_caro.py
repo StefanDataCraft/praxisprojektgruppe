49ca60b7862da306123f9bdb37c218a7fc3442ba
StefanDataCraft <stefan.ronacher@data-craft-students.de>
2023-04-26 12:05:37 +0200

    Stuck in print_members

--- src/aufgabe_6_ohne_caro.py
+++ src/aufgabe_6_ohne_caro.py
@@ -1,5 +1,5 @@
 from datetime import datetime as dt
-
+import pandas as pd
 
 class Person:
     def __init__(self, name):
@@ -10,8 +10,8 @@ class Person:
 
 
 class Student(Person):
-    def __init__(self):
-        pass
+    def __init__(self, name):
+        super().__init__(name)
 
 
 class Dozent(Person):
@@ -54,6 +54,20 @@ class Workshop:
         elif type(person) is Dozent:
             self.dozenten.append(person)
 
+    def print_members(self):
+        studenten_df = pd.DataFrame()
+        dozenten_df = pd.DataFrame()
+        for student in self.studenten:
+            student_to_insert = pd.DataFrame()
+            student_to_insert['name'] = student.name
+            studenten_df.loc[len(studenten_df)] = student_to_insert
+        for dozent in self.dozenten:
+            dozent_to_insert = pd.DataFrame()
+            dozent_to_insert['name'] = dozent.name
+            dozent_to_insert['skills'] = dozent.skill_list
+            dozenten_df.loc[len(dozenten_df)] = dozent_to_insert
+        return (studenten_df, dozenten_df)
+
 
 person1 = Person("Stepsi")
 person1.introduce()
@@ -63,7 +77,7 @@ dozent1.introduce()
 dozent1.add_skill("C++")
 print(dozent1.get_skills())
 
-student1 = Student()
+student1 = Student("Ralf")
 
 workshop = Workshop("20.11.2023","20.12.2023","efeww")
 workshop.add_participants(dozent1)
@@ -74,3 +88,10 @@ for dozent in dozenten_in_workshop:
     print(dozent.name)
 print(workshop.start_date)
 print(workshop.end_date)
+
+studenten_df, dozenten_df = workshop.print_members()
+print("studenten_df:")
+print(studenten_df)
+print("dozenten_df:")
+print(dozenten_df)
+
