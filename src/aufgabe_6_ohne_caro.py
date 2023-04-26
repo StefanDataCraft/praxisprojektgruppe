9513417d6bb39ada6bd70075632243f4d1e447d2
StefanDataCraft <stefan.ronacher@data-craft-students.de>
2023-04-26 13:56:50 +0200

    print_functions done

--- src/aufgabe_6_ohne_caro.py
+++ src/aufgabe_6_ohne_caro.py
@@ -3,26 +3,26 @@ import pywizards as pyw
 person1 = pyw.Person("Stepsi")
 person1.introduce()
 
-dozent1 = pyw.Dozent("Karli", "Ich bin ich, und du bist mein Sofa")
-dozent1.introduce()
-dozent1.add_skill("C++")
-print(dozent1.get_skills())
+professor1 = pyw.Professor("Karli", "Ich bin ich, und du bist mein Sofa")
+professor1.introduce()
+professor1.add_skill("C++")
+print(professor1.get_skills())
 
 student1 = pyw.Student("Ralf", "Reason")
 
 workshop = pyw.Workshop("20.11.2023","20.12.2023","efeww")
-workshop.add_participants(dozent1)
-workshop.add_participants(dozent1)
+workshop.add_participants(professor1)
+workshop.add_participants(professor1)
 workshop.add_participants(student1)
-dozenten_in_workshop = workshop.dozenten
-for dozent in dozenten_in_workshop:
-    print(dozent.name)
+professors_in_workshop = workshop.professors
+for professor in professors_in_workshop:
+    print(professor.name)
 print(workshop.start_date)
 print(workshop.end_date)
 
-studenten_df, dozenten_df = workshop.print_members()
-print("studenten_df:")
-print(studenten_df)
-print("dozenten_df:")
-print(dozenten_df)
+students_df, professors_df = workshop.get_workshops()
+print("students_df:")
+print(students_df)
+print("professors_df:")
+print(professors_df)
 
