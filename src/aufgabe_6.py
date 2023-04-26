22d5fe1ed759e7a62b440413e3b4c162ba94026d
StefanDataCraft <stefan.ronacher@data-craft-students.de>
2023-04-26 11:02:13 +0200

    Before Carolinas intervention

--- src/aufgabe_6.py
+++ src/aufgabe_6.py
@@ -6,6 +6,11 @@ class Person:
         print(f"Hallo, mein Name ist {self.name}")
 
 
+class Student(Person):
+    def __init__(self):
+        pass
+
+
 class Dozent(Person):
     skill_list = []
 
@@ -19,6 +24,23 @@ class Dozent(Person):
     def get_skills(self):
         return self.skill_list
 
+class Workshop:
+    #start_datum
+    #end_datum
+    #thema
+
+    dozenten = []
+    studenten = []
+
+    def __init__(self):
+        pass
+
+    def add_participants(self, person):
+        if type(person) is Student:
+            self.studenten.append(person)
+        elif type(person) is Dozent:
+            self.dozenten.append(person)
+
 
 person1 = Person("Stepsi")
 person1.introduce()
@@ -27,3 +49,13 @@ dozent1 = Dozent("Karli", "Ich bin ich, und du bist mein Sofa")
 dozent1.introduce()
 dozent1.add_skill("C++")
 print(dozent1.get_skills())
+
+student1 = Student()
+
+workshop = Workshop()
+workshop.add_participants(dozent1)
+workshop.add_participants(dozent1)
+workshop.add_participants(student1)
+dozenten_in_workshop = workshop.dozenten
+for dozent in dozenten_in_workshop:
+    print(dozent.name)
